375f14d8a13b5fcc7b910d4b6fc0740a83bdd494
GeorgeFarg <107982159+GeorgeFarg@users.noreply.github.com>
2022-09-05 08:55:48 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@ let myNewElement = document.createElement("div");
 myNewElement.style.textAlign="center";
 
 let image = document.createElement("img");
-image.src = "/images/illustration-thank-you.svg";
-image.alt = "/images/illustration-thank-you.svg";
+image.src = "./images/illustration-thank-you.svg";
+image.alt = "./images/illustration-thank-you.svg";
 
 myNewElement.appendChild(image);
 
